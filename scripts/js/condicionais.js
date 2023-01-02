3cf6f7ce450c94e14f150d80a5762c9995ae396c
Brielon <angelopaca@hotmail.com>
2023-01-02 13:15:19 -0200

    push matinal 02-01-2023

---
+++ scripts/js/condicionais.js
@@ -0,0 +1,9 @@
+let teste = 1;
+if(teste)
+    console.log("teste positivo");
+else
+    console.log("teste negativo");
+// caso o valor de teste seja false o codigo retornara negativo e positivo se teste for true
+console.log("operador ternario");
+let msg = teste ? "o valor de teste e true" : "o valor de teste e falce"
+console.log(msg);

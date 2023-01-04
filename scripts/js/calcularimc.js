38c433f3298432ae7418f00a02a56903d937a32f
Brielon <angelopaca@hotmail.com>
2023-01-04 14:22:46 -0200

    aulas js adicionadas

---
+++ scripts/js/calcularimc.js
@@ -0,0 +1,19 @@
+//requisitos
+//Deve receber um numero o iMC
+//reve retornar uma string
+//deve retornar um erro se receceber um parametro nao numero 
+//deve retornar erro caso nao receba nenhum parametro
+//classificaçao                     imc
+//muito abaixo do peso                  16 a 16,9kg/m2
+//abaixo do peso                        17 a 18,4kg/m2
+//peso normal                           18,5 a 24,9kg/m2
+//acima do peso                         25 a 29,9kg/m2
+//obesidade de grau I                   30 a 34,9kg/m2
+//obesidade de grau II                  35 a 40kg/m2    
+//obesidade de grau III                 maior que 40kg/m2
+(function(){
+    function imcCalculo(peso,altura) {
+        
+    }
+
+})()

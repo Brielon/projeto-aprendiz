d4356a30334e9667abd9531a9b5f39601853dca8
Brielon <angelopaca@hotmail.com>
2023-01-04 15:51:52 -0200

    Entrando no DOM

--- scripts/js/calcularimc.js
+++ scripts/js/calcularimc.js
@@ -11,9 +11,52 @@
 //obesidade de grau I                   30 a 34,9kg/m2
 //obesidade de grau II                  35 a 40kg/m2    
 //obesidade de grau III                 maior que 40kg/m2
+// IMC = peso / (altura x altura)
 (function(){
-    function imcCalculo(peso,altura) {
-        
+
+    //funcao de callback classifica o imc
+        const cb = function(IMC) {    
+        let msg = ""
+        if(IMC<17){
+            //muito baixo
+            msg += `Muito abaixo do peso`
+        }else if(IMC<18.5){
+            //abaixo do peso
+            msg += `Abaixo do peso`
+        }else if(IMC<25){
+            //peso normal
+            msg += `Peso normal`   
+        }else if(IMC<30){
+            //acima do peso
+            msg += `Acima do peso`
+        }else if(IMC<35){
+            //obesidade de grau I
+            msg += `Obesidade de grau I`  
+        } else if (IMC<40) {
+            //obesidade de grau II
+            msg += `Obesidade de grau II`
+        } else if (IMC>=40) {
+            //obesidade de grau III
+            msg += `Obesidade de grau III`   
+        }
+        return msg
     }
+
+
+
+    function imcCalculoPA(peso,altura,cb) {
+        if(peso === undefined || altura === undefined){
+            throw Error("Dados icorretos")
+        }
+        let IMC = peso / (altura * altura)
+         if (arguments[2]==undefined) {
+             return IMC.toFixed(1)    
+        }else{
+        return `O IMC calculado foi ${IMC.toFixed(1)} ${cb(IMC)}`
+    }
+    }
+    let mensagem2 = imcCalculoPA(70,2,cb)
+    let mensagem = imcCalculoPA(70,2)
+    console.log(mensagem,mensagem2)
 
 })()

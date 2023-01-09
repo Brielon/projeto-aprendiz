ee68b3ec818ae2ec12e21b0ccdac367f518f572d
Angelo Gabriel Soares dos Santos <angelo.santos@mv.com.br>
2023-01-09 15:40:07 -0300

    Atualizando 09-01-23

--- scripts/DOMjs/ex_saudaçao/script/script.js
+++ scripts/DOMjs/ex_saudaçao/script/script.js
@@ -1,6 +1,19 @@
 
-    const nome = "Ana"
-    let saudaçao = document.querySelector(".top-bar p")
-    saudaçao.textContent = saudaçao.textContent+nome
+(function(){
+let nome = null
+if(typeof(nome)=="string"){
+    let hero=document.querySelector(".hero"),
+        h_content=document.querySelector(".hero-content")
+//hero.innerHTML = `<div class="top-bar"><p>Bem-vindo(a),${nome}</p></div>${hero.innerHTML}`
+let div = document.createElement("div")
+div.className = "top-bar"
+hero.insertBefore(nome,h_content)
+let nome = document.createElement("p")
+div.insertAdjacentHTML(nome)
+
+}
+let teste = document.querySelector("main")
+teste.className = "teste"
+
     
-    
+}) ()

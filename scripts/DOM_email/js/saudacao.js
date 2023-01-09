0ff625039349c1a15e3ee877e06b6d128996e15c
Angelo Gabriel Soares dos Santos <angelo.santos@mv.com.br>
2023-01-09 17:13:37 -0300

    testes

--- scripts/DOM_email/js/saudacao.js
+++ scripts/DOM_email/js/saudacao.js
@@ -1,4 +1,10 @@
-(function () {
 
+let email = document.querySelector("#txtEmail")
+let enviar = function() {
+    let conteudo = email.value,
+        feedback = document.querySelector("#newsletterFeedback")
+        feedback.innerHTML = `<p>O email ${conteudo}, foi cadastrado com sucesso</p>`
+        email.value = ""
+        
+}
 
-})()

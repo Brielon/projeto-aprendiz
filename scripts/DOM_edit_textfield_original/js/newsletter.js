9a5ea6886b221c2353ae71ca3bf9126a3f07f6e6
Angelo Gabriel Soares dos Santos <angelo.santos@mv.com.br>
2023-01-10 08:53:35 -0300

    matinal 10-01-23

--- scripts/DOM_edit_textfield_original/js/newsletter.js
+++ scripts/DOM_edit_textfield_original/js/newsletter.js
@@ -1,4 +1,11 @@
 const txtEmail = document.getElementById("txtEmail")
-
-
+//editar
+const semclick = function(){
+    txtEmail.disabled = true
+}
+const editar = function(){
+        
+    txtEmail.disabled = false
+    txtEmail.focus()
+}
 

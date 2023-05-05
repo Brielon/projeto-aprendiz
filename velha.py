a0bdd27593c39edd0c8c5f2264bd5db5d4450994
Angelo <angelo.santos@mv.com.br>
2023-05-05 17:46:01 -0300

    Falta inteligencia para saber quandp ganha

--- velha.py
+++ velha.py
@@ -1,31 +1,29 @@
-global folhaEmBranco = """
- 1 | 2 | 3
----+---+----
- 4 | X | 6
----+---+----
- 7 | 8 | 9
-"""
-
-import randon
+import random
 
 def TurnoAdversario(folha):
-    bolinha = str(int(randon.randon()*9))
+    bolinha = str(int(random.random()*9))
     if (bolinha in folha):
-        folha.replase(bolinha,"X")
+        folha = folha.replace(bolinha,"X")
         JogadorTurn(folha)
-     else:
+    else:
         TurnoAdversario(folha)
 
 def JogadorTurn(folha):
     print(folha,"marque 'O'(1-9):")
     bolinha = input()
     if (bolinha in folha):
-        folha.replase(bolinha,"O")
+        folha = folha.replace(bolinha,"O")
     else:
         print("Opção invalida")
         JogadorTurn(folha)
     TurnoAdversario(folha)
 
-def iniciar(folha = folhaEmBranco):
-    JogadorTurn(folha)
+def iniciar():
+    JogadorTurn("""
+ 1 | 2 | 3
+---+---+----
+ 4 | X | 6
+---+---+----
+ 7 | 8 | 9
+""")
 iniciar()

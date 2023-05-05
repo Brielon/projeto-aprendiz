f253c33f76fae4bf39f3b821150428c8914c9a2e
Angelo <119819586+Brielon@users.noreply.github.com>
2023-05-05 17:19:13 -0300

    Update velha.py
    versao3

--- velha.py
+++ velha.py
@@ -6,12 +6,25 @@ global folhaEmBranco = """
  7 | 8 | 9
 """
 
+import randon
+
+def TurnoAdversario(folha):
+    bolinha = str(int(randon.randon()*9))
+    if (bolinha in folha):
+        folha.replase(bolinha,"X")
+        JogadorTurn(folha)
+     else:
+        TurnoAdversario(folha)
+
 def JogadorTurn(folha):
     print(folha,"marque 'O'(1-9):")
     bolinha = input()
     if (bolinha in folha):
         folha.replase(bolinha,"O")
-    
+    else:
+        print("Opção invalida")
+        JogadorTurn(folha)
+    TurnoAdversario(folha)
 
 def iniciar(folha = folhaEmBranco):
     JogadorTurn(folha)
